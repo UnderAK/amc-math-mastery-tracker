7969fd193242fd0bece3343616a4b09f00578f76
Aarav Tibrewal <aarav@Rachnas-MacBook-Air.local>
2025-07-23 17:07:35 +0530

    yay

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-btn-border bg-transparent backdrop-blur-md text-btn-foreground hover:bg-btn-background-hover",
         secondary:
-          "bg-btn-primary-background border border-transparent backdrop-blur-lg shadow-inner-blue-sm hover:bg-btn-primary-background-hover text-btn-colored-foreground",
+          "bg-btn-primary-background border border-transparent backdrop-blur-lg shadow-inner-blue-sm hover:bg-btn-primary-background-hover",
         ghost: "hover:bg-btn-background-hover text-btn-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         gradient: "text-primary-foreground dark:text-secondary-foreground border-transparent bg-transparent",
