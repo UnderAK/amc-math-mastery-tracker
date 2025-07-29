2315a16a0370a2f3bef638e45392280ffa1a69f5
Aarav Tibrewal <aarav@Rachnas-MacBook-Air.local>
2025-07-29 19:19:17 +0530

    yay

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { MainLayout } from "@/components/MainLayout";
 import Index from "./pages/Index";
 import Analytics from "./pages/Analytics";
@@ -79,31 +79,29 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ScoringModeProvider>
-        <BrowserRouter>
-          <AuthProvider value={{ session, isGuest, handleLogout }}>
-            <TooltipProvider>
-              <Toaster />
-              <Sonner />
-              {!isOnline && <OfflineBanner />}
-              <Routes>
-                <Route element={<MainLayout />}>
-                  <Route path="/" element={<Index />} />
-                  <Route path="/analytics" element={<Analytics />} />
-                  <Route path="/test-entry" element={<TestEntryPage />} />
-                  <Route path="/practice" element={<PracticePage />} />
-                  <Route path="/history" element={<History />} />
-                  <Route path="/leaderboard" element={<Leaderboard />} />
-                </Route>
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-              {/* Konami code toast */}
-              <KonamiToast />
-              <VercelAnalytics />
-              <SpeedInsights />
-            </TooltipProvider>
-          </AuthProvider>
-        </BrowserRouter>
+        <AuthProvider value={{ session, isGuest, handleLogout }}>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            {!isOnline && <OfflineBanner />}
+            <Routes>
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<Index />} />
+                <Route path="/analytics" element={<Analytics />} />
+                <Route path="/test-entry" element={<TestEntryPage />} />
+                <Route path="/practice" element={<PracticePage />} />
+                <Route path="/history" element={<History />} />
+                <Route path="/leaderboard" element={<Leaderboard />} />
+              </Route>
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            {/* Konami code toast */}
+            <KonamiToast />
+            <VercelAnalytics />
+            <SpeedInsights />
+          </TooltipProvider>
+        </AuthProvider>
       </ScoringModeProvider>
     </QueryClientProvider>
   );
