a2b852e2d73d09090d5dd1ffca5359ee7336fbb7
Aarav Tibrewal <aarav@Rachnas-MacBook-Air.local>
2025-07-29 19:18:18 +0530

    fix: remove nested BrowserRouter

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-706c6701'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.50u7222bh34"
+    "revision": "0.svpc5hodojk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
