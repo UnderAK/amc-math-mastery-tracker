6d39a68efa9ec432436f97aaa089192303a01740
Aarav Tibrewal <aarav@Rachnas-MacBook-Air.local>
2025-07-29 19:24:34 +0530

    updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-706c6701'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.g490c10lo34"
+    "revision": "0.cr8e4qs1g6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
